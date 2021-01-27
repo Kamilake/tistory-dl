6707b23b378277753feadeda336e0933d0f9078f
Kamilake <exjang0@gmail.com>
2021-01-27 09:10:40 +0900

    Update Log.java

--- src/mainmain/Log.java
+++ src/mainmain/Log.java
@@ -24,9 +24,9 @@ public class Log {
         return 0;
     }
 	/** sysout 출력하기 전에 이 매소드를 거쳐가면 텍스트 파일로 로그를 찍게 된다. 아직 저장은 미구현이라 sysout과 동일한 기능만을 제공. */
-	public int println(Exception e2){
+	public int println(Exception string){
         try{
-        	System.out.println(e2);
+        	System.out.println(string);
         }catch(Exception e){
             System.out.println("로그를 저장할 수 없음: "+e);
         }
