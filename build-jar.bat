1940120d1e4ee227a766289f44d42f4fd682e427
Kamilake <exjang0@gmail.com>
2021-12-09 09:39:27 +0900

    .

--- build-jar.bat
+++ build-jar.bat
@@ -1,10 +1,8 @@
 chcp 65001
-rm -r build-jar/*
 rmdir /s /q build-jar
-rmdir -r build-jar
 mkdir build-jar
 unzip -o ./libs/*.jar -d build-jar
 javac -cp libs/* -encoding utf-8 -d ./build-jar src/mainmain\*
 chcp 949
 jar -cfve tistory-dl.jar mainmain.Backup -C build-jar .
-pause
+rmdir /s /q build-jar
